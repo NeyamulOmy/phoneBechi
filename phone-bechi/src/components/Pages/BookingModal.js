8469e205992985cfb62933e0e7719e3a81261fa1
NeyamulOmy <neyamul.omy@gmail.com>
2022-11-28 13:32:12 +0600

    booking modal

--- phone-bechi/src/components/Pages/BookingModal.js
+++ phone-bechi/src/components/Pages/BookingModal.js
@@ -2,7 +2,28 @@ import React, { useContext } from 'react';
 import { AuthContext } from '../../contexts/AuthProvider'
 const BookingModal = ({ product }) => {
     const { user } = useContext(AuthContext)
-    const { name } = product;
+    const { name, askPrice } = product;
+    const handleBooking = event => {
+        event.preventDefault();
+        const form = event.target;
+        const productName = form.productName.value;
+        const price = form.price.value;
+        const userName = form.name.value;
+        const userEmail = form.email.value;
+        const phone = form.phone.value;
+        const location = form.location.value;
+
+        const booking = {
+            name: productName,
+            price: price,
+            buyer: userName,
+            email: userEmail,
+            phone: phone,
+            location: location
+        }
+        console.log(booking)
+
+    }
     return (
         <>
 
@@ -12,9 +33,17 @@ const BookingModal = ({ product }) => {
             <div className="modal">
                 <div className="modal-box relative">
                     <label htmlFor="bookingModal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-                    <h3 className="text-lg font-bold">Book {name}!</h3>
-                    <h4 className='text-md text-yellow-800'>Buyer: {user.displayName}</h4>
-                    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                    <h3 className="text-lg font-bold">Book Item!</h3>
+                    <form onSubmit={handleBooking} className='grid grid-cols-1 gap-3 mt-10'>
+                        <input type="text" disabled className='input w-full input-bordered' name="productName" defaultValue={name} />
+                        <input type="text" disabled className='input w-full input-bordered' name="price" defaultValue={`${askPrice} TK`} />
+                        <input name="name" type="text" defaultValue={user?.displayName} disabled placeholder="Your Name" className="input w-full input-bordered" />
+                        <input name="email" type="email" defaultValue={user?.email} disabled placeholder="Email Address" className="input w-full input-bordered" />
+                        <input name="phone" type="text" placeholder="Phone Number" className="input w-full input-bordered" />
+                        <input name="location" type="text" placeholder='Meeting location' className="input w-full input-bordered" />
+                        <br />
+                        <input className='btn btn-accent w-full' type="submit" value="Book" />
+                    </form>
                 </div>
             </div>
         </>

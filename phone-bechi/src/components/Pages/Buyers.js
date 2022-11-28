dec59e1a6f3cca83a646fe7429eb9bdc44f50fa4
NeyamulOmy <neyamul.omy@gmail.com>
2022-11-28 18:51:30 +0600

    showing buyers and sellers

--- phone-bechi/src/components/Pages/Buyers.js
+++ phone-bechi/src/components/Pages/Buyers.js
@@ -1,9 +1,48 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const Buyers = () => {
+    const url = 'http://localhost:5000/buyers';
+    const { data: buyers = [] } = useQuery({
+        queryKey: ['buyers'],
+        queryFn: async () => {
+            const res = await fetch(url, {
+                headers: {
+
+                }
+            });
+            const data = await res.json();
+            console.log(data)
+            return data;
+        }
+    })
     return (
         <div>
+            <h3 className="text-3xl mb-5">Buyers</h3>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            buyers &&
+                            buyers.map((buyers, i) => <tr key={i}>
+                                <th>{i + 1}</th>
+                                <td><h1>{buyers.name}</h1></td>
+                                <td>{buyers.email}</td>
+                                <td></td>
 
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
--- phone-bechi/src/components/Pages/Sellers.js
+++ phone-bechi/src/components/Pages/Sellers.js
@@ -1,9 +1,48 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const Sellers = () => {
+    const url = 'http://localhost:5000/sellers';
+    const { data: sellers = [] } = useQuery({
+        queryKey: ['sellers'],
+        queryFn: async () => {
+            const res = await fetch(url, {
+                headers: {
+
+                }
+            });
+            const data = await res.json();
+            console.log(data)
+            return data;
+        }
+    })
     return (
         <div>
+            <h3 className="text-3xl mb-5">Seller</h3>
+            <div className="overflow-x-auto">
+                <table className="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            sellers &&
+                            sellers.map((seller, i) => <tr key={i}>
+                                <th>{i + 1}</th>
+                                <td><h1>{seller.name}</h1></td>
+                                <td>{seller.email}</td>
+                                <td></td>
 
+                            </tr>)
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
